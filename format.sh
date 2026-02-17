cb00f555e5bbb9ab60bfa7a364638cedffc28319
Sy Le <lenguyensy@gmail.com>
2026-02-17 09:45:02 -0800

    Update format.sh

--- format.sh
+++ format.sh
@@ -13,17 +13,35 @@ FORMAT_COMMAND_TO_RUN: $FORMAT_COMMAND_TO_RUN
 """
 
 # NOTE: refer to https://github.com/synle/bashrc/blob/master/.build/format for the latest
+# ----------------------------------------------------
+# Light Cleanup (depth limited)
+# ----------------------------------------------------
 function format_cleanup_light {
+
   local base_dir="${1:-.}"
-  local max_depth=4
+  local max_depth=6
 
   if [ ! -d "$base_dir" ]; then
     return 1
   fi
 
-  local count=$(find "$base_dir" \
+  find "$base_dir" \
     -maxdepth "$max_depth" \
-    -type f \( -name '*.Identifier' -o -name '._*' \) \
+    \( \
+      -type f \( \
+        -name '*.Identifier' -o \
+        -name '._*' -o \
+        -name '.DS_Store' -o \
+        -name '.AppleDouble' -o \
+        -name '.LSOverride' -o \
+        -name 'Icon?' \
+      \) -o \
+      -type d \( \
+        -name '.Spotlight-V100' -o \
+        -name '.Trashes' -o \
+        -name '.fseventsd' \
+      \) \
+    \) \
     -not -path '*/__pycache__/*' \
     -not -path '*/.cache/*' \
     -not -path '*/.ebextensions/*' \
@@ -45,41 +63,16 @@ function format_cleanup_light {
     -not -path '*/tmp/*' \
     -not -path '*/venv/*' \
     -not -path '*/webpack-dist/*' \
-    -print | wc -l)
-
-  if [ "$count" -gt 0 ]; then
-    find "$base_dir" \
-      -maxdepth "$max_depth" \
-      -type f \( -name '*.Identifier' -o -name '._*' \) \
-      -not -path '*/__pycache__/*' \
-    -not -path '*/.cache/*' \
-    -not -path '*/.ebextensions/*' \
-    -not -path '*/.generated/*' \
-    -not -path '*/.git/*' \
-    -not -path '*/.gradle/*' \
-    -not -path '*/.hg/*' \
-    -not -path '*/.idea/*' \
-    -not -path '*/.mypy_cache/*' \
-    -not -path '*/.pytest_cache/*' \
-    -not -path '*/.sass-cache/*' \
-    -not -path '*/.svn/*' \
-    -not -path '*/bower_components/*' \
-    -not -path '*/build/*' \
-    -not -path '*/coverage/*' \
-    -not -path '*/CVS/*' \
-    -not -path '*/dist/*' \
-    -not -path '*/node_modules/*' \
-    -not -path '*/tmp/*' \
-    -not -path '*/venv/*' \
-    -not -path '*/webpack-dist/*' \
-      -delete
-  fi
+    -exec rm -rf {} +
 }
 
-function format_other_text_based_files {
-  echo '>> Formatting All Text-Based Files...'
 
-  # Configuration: Add folders or files you want to skip
+# ----------------------------------------------------
+# Text File Formatting
+# ----------------------------------------------------
+function format_other_text_based_files {
+  echo '>> Formatting text-based files...'
+
   EXCLUDE_DIRS=(
     ".git"
     "node_modules"
@@ -90,7 +83,7 @@ function format_other_text_based_files {
     ".next"
     "venv"
     ".venv"
-    "target" # Common for Rust/Java
+    "target"
   )
 
   EXCLUDE_FILES=(
@@ -102,41 +95,28 @@ function format_other_text_based_files {
     ".DS_Store"
   )
 
-  # Build the directory prune arguments
   dir_args=()
   for i in "${!EXCLUDE_DIRS[@]}"; do
     dir_args+=("-name" "${EXCLUDE_DIRS[$i]}")
-    if [ $i -lt $((${#EXCLUDE_DIRS[@]} - 1)) ]; then
-      dir_args+=("-o")
-    fi
+    [ $i -lt $((${#EXCLUDE_DIRS[@]} - 1)) ] && dir_args+=("-o")
   done
 
-  # Build the file exclusion arguments
   file_exclude_args=()
   for i in "${!EXCLUDE_FILES[@]}"; do
     file_exclude_args+=("-name" "${EXCLUDE_FILES[$i]}")
-    if [ $i -lt $((${#EXCLUDE_FILES[@]} - 1)) ]; then
-      file_exclude_args+=("-o")
-    fi
+    [ $i -lt $((${#EXCLUDE_FILES[@]} - 1)) ] && file_exclude_args+=("-o")
   done
 
-  # find .
-  # 1. Prune the excluded directories
-  # 2. Filter out specific excluded files
-  # 3. Check MIME type for text files
-  find . -type d \( "${dir_args[@]}" \) -prune -o -type f ! \( "${file_exclude_args[@]}" \) -print | while read -r file; do
+  find . -type d \( "${dir_args[@]}" \) -prune -o     -type f ! \( "${file_exclude_args[@]}" \) -print |     while read -r file; do
 
-    if file --mime-type "$file" | grep -q "text/"; then
-      echo "Formatting: $(readlink -f "$file")"
-
-      # Removes trailing whitespace
-      sed -i 's/[ \t]*$//' "$file"
-    fi
-
-  done
+      if file --mime-type "$file" | grep -q "text/"; then
+        sed -i 's/[ \t]*$//' "$file"
+      fi
+    done
 
   echo '>> DONE Formatting All Text-Based Files'
 }
+
 
 ############################################
 format_cleanup_light
